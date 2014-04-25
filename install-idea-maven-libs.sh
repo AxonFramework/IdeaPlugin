f79d914fa9500e482791c66762b7d4a0a484ad3e
Allard Buijze <buijze@gmail.com>
2014-04-25 15:54:57 +0200

    Moved plugin.xml to module root for intellij to recognize it. Also changed shell script to run it on windows (in Git Bash)

--- install-idea-maven-libs.sh
+++ install-idea-maven-libs.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-export IDEA="/Applications/IntelliJ IDEA 12.app"
-export IDEA_VERSION=12.0.4
+export IDEA="/C/Program Files (x86)/JetBrains/IntelliJ IDEA 13.1"
+if [ ! -e "$IDEA" ]
+then
+    export IDEA="/Applications/IntelliJ IDEA 13.app"
+fi
+
+export IDEA_VERSION=13.1.2
 mvn install:install-file -Dfile="$IDEA/lib/annotations.jar" -DgroupId=com.intellij.idea -DartifactId=annotations -Dversion=$IDEA_VERSION -Dpackaging=jar
 mvn install:install-file -Dfile="$IDEA/lib/idea.jar" -DgroupId=com.intellij.idea -DartifactId=idea -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=$IDEA_VERSION
 mvn install:install-file -Dfile="$IDEA/lib/openapi.jar" -DgroupId=com.intellij.idea -DartifactId=openapi -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=$IDEA_VERSION
