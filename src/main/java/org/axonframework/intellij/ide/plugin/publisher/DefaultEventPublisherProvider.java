8056d294cb90fad3e369e8ef2e1d0be28a699f1a
Rino <rino.kadijk@salvesdevelopment.nl>
2014-08-14 21:54:07 +0200

    small refactoring and updated release notes

--- src/main/java/org/axonframework/intellij/ide/plugin/publisher/DefaultEventPublisherProvider.java
+++ src/main/java/org/axonframework/intellij/ide/plugin/publisher/DefaultEventPublisherProvider.java
@@ -2,43 +2,36 @@ package org.axonframework.intellij.ide.plugin.publisher;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Condition;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiMethodCallExpression;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.PsiType;
+import com.intellij.psi.*;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.Processor;
 import com.intellij.util.Query;
+import com.intellij.util.containers.ConcurrentMultiMap;
+import com.intellij.util.containers.MultiMap;
 
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
+
+import static java.util.Arrays.asList;
 
 class DefaultEventPublisherProvider implements EventPublisherProvider {
 
-    private final Map<Project, List<PsiMethod>> publisherMethodsPerProject = new ConcurrentHashMap<Project, List<PsiMethod>>();
+    private MultiMap<Project, PsiMethod> publisherMethodsPerProject = new ConcurrentMultiMap<Project, PsiMethod>();
 
     @Override
     public void scanPublishers(Project project, GlobalSearchScope scope, final Registrar registrar) {
         cleanClosedProjects();
-        final CopyOnWriteArrayList<PsiMethod> methods = new CopyOnWriteArrayList<PsiMethod>();
-        publisherMethodsPerProject.put(project, methods);
-        methods.addAll(findMethods(project, GlobalSearchScope.allScope(project),
-                                   "org.axonframework.eventsourcing.AbstractEventSourcedAggregateRoot", "apply"));
-        methods.addAll(findMethods(project, GlobalSearchScope.allScope(project),
-                                   "org.axonframework.domain.AbstractAggregateRoot", "registerEvent"));
-        methods.addAll(findMethods(project, GlobalSearchScope.allScope(project),
-                                   "org.axonframework.eventsourcing.AbstractEventSourcedEntity", "apply"));
+        publisherMethodsPerProject.putValues(project, findMethods(project, GlobalSearchScope.allScope(project),
+                "org.axonframework.eventsourcing.AbstractEventSourcedAggregateRoot", "apply"));
+        publisherMethodsPerProject.putValues(project, findMethods(project, GlobalSearchScope.allScope(project),
+                "org.axonframework.domain.AbstractAggregateRoot", "registerEvent"));
+        publisherMethodsPerProject.putValues(project, findMethods(project, GlobalSearchScope.allScope(project),
+                "org.axonframework.eventsourcing.AbstractEventSourcedEntity", "apply"));
 
-        for (PsiMethod method : methods) {
+        for (PsiMethod method : publisherMethodsPerProject.values()) {
             Query<PsiReference> invocations = ReferencesSearch.search(method, scope);
             invocations.forEachAsync(new Processor<PsiReference>() {
                 @Override
@@ -83,14 +76,14 @@ class DefaultEventPublisherProvider implements EventPublisherProvider {
                 className,
                 allScope);
         if (aggregateClass != null) {
-            return Arrays.asList(aggregateClass.findMethodsByName(methodName, true));
+            return asList(aggregateClass.findMethodsByName(methodName, true));
         }
         return Collections.emptyList();
     }
 
     @Override
     public EventPublisher resolve(PsiElement element) {
-        List<PsiMethod> methods = publisherMethodsPerProject.get(element.getProject());
+        Collection<PsiMethod> methods = publisherMethodsPerProject.get(element.getProject());
         if (element instanceof PsiMethodCallExpression) {
             PsiMethodCallExpression expression = (PsiMethodCallExpression) element;
             PsiType[] expressionTypes = expression.getArgumentList().getExpressionTypes();
