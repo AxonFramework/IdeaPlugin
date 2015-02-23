768d5b63b485acc0efc26d000a34e387a605c721
Rino <rino.kadijk@salvesdevelopment.nl>
2015-02-23 07:57:22 +0100

    Enabled disabled feature IDEAPLUGIN-4

--- src/main/java/org/axonframework/intellij/ide/plugin/handler/AnnotationTypes.java
+++ src/main/java/org/axonframework/intellij/ide/plugin/handler/AnnotationTypes.java
@@ -3,8 +3,8 @@ package org.axonframework.intellij.ide.plugin.handler;
 public enum AnnotationTypes {
     EVENT_HANDLER("org.axonframework.eventhandling.annotation.EventHandler", "@EventHandler"),
     EVENT_SOURCING_HANDLER("org.axonframework.eventsourcing.annotation.EventSourcingHandler", "@EventSourcingHandler"),
-    SAGA_EVENT_HANDLER("org.axonframework.saga.annotation.SagaEventHandler", "@SagaEventHandler", "associationProperty");
-//    COMMAND_EVENT_HANDLER("org.axonframework.commandhandling.annotation.CommandHandler", "@CommandHandler");
+    SAGA_EVENT_HANDLER("org.axonframework.saga.annotation.SagaEventHandler", "@SagaEventHandler", "associationProperty"),
+    COMMAND_EVENT_HANDLER("org.axonframework.commandhandling.annotation.CommandHandler", "@CommandHandler");
 
     private final String fullyQualifiedName;
     private final String annotation;
@@ -33,7 +33,6 @@ public enum AnnotationTypes {
     }
 
     public boolean isCommand() {
-        return false;
-//        return this.annotation.equals("@CommandHandler");
+        return this.annotation.equals("@CommandHandler");
     }
 }
