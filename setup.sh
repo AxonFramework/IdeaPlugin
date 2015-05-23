79879e53d8f9807bd7613c349cbd47f3caf7d1de
Rino <r.kadijk@portofrotterdam.com>
2015-05-23 07:48:48 +0200

    Upgraded to new IDEA version and adjusted release goal

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-export IDEA="/C/Program Files (x86)/JetBrains/IntelliJ IDEA 14.0"
+export IDEA="/C/Program Files (x86)/JetBrains/IntelliJ IDEA 14.1"
 if [ ! -e "$IDEA" ]
 then
     export IDEA="/Applications/IntelliJ IDEA 14.app/Contents"
 fi
 
-export IDEA_VERSION=14.0.3
+export IDEA_VERSION=14.1.3
 mvn install:install-file -Dfile="$IDEA/lib/annotations.jar" -DgroupId=com.intellij.idea -DartifactId=annotations -Dversion=$IDEA_VERSION -Dpackaging=jar
 mvn install:install-file -Dfile="$IDEA/lib/idea.jar" -DgroupId=com.intellij.idea -DartifactId=idea -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=$IDEA_VERSION
 mvn install:install-file -Dfile="$IDEA/lib/openapi.jar" -DgroupId=com.intellij.idea -DartifactId=openapi -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=$IDEA_VERSION
